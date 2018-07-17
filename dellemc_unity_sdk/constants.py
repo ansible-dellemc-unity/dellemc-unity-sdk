3d24c73a238077bffcfa29e5af769abc3dc4d62a
Andrew Petrov (marsofandrew) <marsofandrew@gmail.com>
2018-07-17 12:37:15 +0300

    remove old constants

--- dellemc_unity_sdk/constants.py
+++ dellemc_unity_sdk/constants.py
@@ -2,16 +2,11 @@
 from enum import Enum
 
 EXECUTED_BY = "executed_by"
-EXECUTED_BY_KEY = EXECUTED_BY  # TODO: remove later
 EXECUTED_BY_SDK = "executed_by_sdk"
 ACTION_TYPE = "action_type"
-ACTION_TYPE_KEY = ACTION_TYPE
 PARAMETER_TYPES = "parameter_types"
-PARAMETER_TYPES_KEY = PARAMETER_TYPES
 REST_OBJECT = "rest_object"
-REST_OBJECT_KEY = REST_OBJECT  # TODO: remove later
 ACTIONS = "actions"
-ACTIONS_KEY = ACTIONS  # TODO: remove later
 ACTION_NAME = "function"
 DO_ACTION = "do_action"
 GET = "get"
@@ -21,6 +16,7 @@ VALIDATOR_MESSAGE = "message"
 ERR_MISSING_REQUIRED_PARAMETER = "Required parameter {} was not found."
 ERR_UNSUPPORTED_PARAMETER = "{} is unsupported parameter"
 ERR_WRONG_TYPE = "{} must be {}, not {}"
+
 
 class ActionType(Enum):
     UPDATE = "update"
